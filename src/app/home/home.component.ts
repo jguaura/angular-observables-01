189c7c2bc945a8d13061458ab4a61888b900be3e
Jose Rafael Guaura <jguaura@gmail.com>
2023-03-11 17:22:17 -0500

    chore:
    - rxjs operators

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { interval, Subscription, Observable } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -19,28 +20,37 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count);
-        if(count === 2) {
-          observer.complete()
+        if (count === 2) {
+          observer.complete();
         }
-        if(count > 3) {
-          observer.error(new Error('Count is greater than 3'))
+        if (count > 3) {
+          observer.error(new Error('Count is greater than 3'));
         }
         count++;
       }, 1000);
     });
 
-    this.dumSubscription = customIntervalObservable.subscribe(
-      (data) => {
-        console.log(data);
-      },
-      (error) => {
-        console.log(error);
-        alert(error.message);
-      },
-      () => {
-        console.log('completed')
-      }
-    );
+    this.dumSubscription = customIntervalObservable
+      .pipe(
+        filter((data) => {
+          return data > 0;
+        }),
+        map((data: number) => {
+          return `Round ${data + 1}`;
+        })
+      )
+      .subscribe(
+        (data) => {
+          console.log(data);
+        },
+        (error) => {
+          console.log(error);
+          alert(error.message);
+        },
+        () => {
+          console.log('completed');
+        }
+      );
   }
 
   ngOnDestroy(): void {
